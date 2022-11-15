0c45f30e24e158d502a1a872b7f324def3616adb
MaximeB3N <maxime.bonnin.pro@gmail.com>
2022-11-15 13:12:48 +0100

    First exploration

---
+++ utils.py
@@ -0,0 +1,38 @@
+import torch
+
+
+def train(model, train_loader, optimizer, use_cuda, epoch, log_interval=10):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if use_cuda:
+            data, target = data.cuda(), target.cuda()
+        optimizer.zero_grad()
+        output = model(data)
+        criterion = torch.nn.CrossEntropyLoss(reduction='mean')
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data.item()))
+
+def validation(model, val_loader, use_cuda):
+    model.eval()
+    validation_loss = 0
+    correct = 0
+    for data, target in val_loader:
+        if use_cuda:
+            data, target = data.cuda(), target.cuda()
+        output = model(data)
+        # sum up batch loss
+        criterion = torch.nn.CrossEntropyLoss(reduction='mean')
+        validation_loss += criterion(output, target).data.item()
+        # get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    validation_loss /= len(val_loader.dataset)
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        validation_loss, correct, len(val_loader.dataset),
+        100. * correct / len(val_loader.dataset)))
